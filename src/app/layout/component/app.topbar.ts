41924f2ce26203e7949fb96166168228f3f8e2a8
pareshsutariya <paresh.sutariya.it@gmail.com>
2025-04-30 17:03:34 -0400

    topbar

--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -6,12 +6,14 @@ import { StyleClassModule } from 'primeng/styleclass';
 import { AppConfigurator } from './app.configurator';
 import { LayoutService } from '../service/layout.service';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector: 'app-topbar',
     standalone: true,
     imports: [ProgressSpinnerModule, RouterModule, CommonModule, StyleClassModule],
-    template: ` <div class="layout-topbar">
+    template: `
+    <div class="layout-topbar">
         <div class="layout-topbar-logo-container">
             <button class="layout-menu-button layout-topbar-action" (click)="layoutService.onMenuToggle()">
                 <i class="pi pi-bars"></i>
@@ -24,6 +26,31 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner';
                 <!-- <span class="whitespace-nowrap">{{layoutService.pageTitle()}}</span> -->
             </a>
         </div>
+
+        <div class="layout-topbar-actions">
+            <div class="layout-config-menu">
+                <div *ngIf="authService.IsUserLoggedIn()" class="whitespace-nowrap">
+                    <button type="button" class="layout-topbar-action" style="color:blue">
+                        <i class="pi pi-user"></i>
+                    </button>
+                </div> 
+            </div>
+
+            <span class="layout-topbar-logo" style="font-size: 1.2rem;">{{authService.LoggedInUserFirstName()}}</span>
+
+            <button class="layout-topbar-menu-button layout-topbar-action" pStyleClass="@next" enterFromClass="hidden" enterActiveClass="animate-scalein" leaveToClass="hidden" leaveActiveClass="animate-fadeout" [hideOnOutsideClick]="true">
+                <i class="pi pi-ellipsis-v"></i>
+            </button>
+
+            <div class="layout-topbar-menu hidden lg:block">
+                <div class="layout-topbar-menu-content">
+                    <button type="button" class="layout-topbar-action" style="color:brown" (click)="authService.logoutUserAndRedirect()">
+                        <i class="pi pi-sign-out"></i>
+                        <span>Logout</span>
+                    </button>
+                </div>
+            </div> 
+        </div>
     </div>
     
     <div class="progress-spinner" *ngIf="layoutService.isDataLoading()">
@@ -34,7 +61,7 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner';
 export class AppTopbar {
     items!: MenuItem[];
 
-    constructor(public layoutService: LayoutService) {}
+    constructor(public layoutService: LayoutService, public authService: AuthService) {}
 
     toggleDarkMode() {
         this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
